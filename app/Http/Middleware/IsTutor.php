8315c30632185dd3f6e6830a91748dd019fcb884
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2022-02-16 14:23:32 +0700

    add IsTutor middleware

---
+++ app/Http/Middleware/IsTutor.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class IsTutor {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next) {
+        if (Auth::user() && Auth::user()->is_tutor) {
+            return $next($request);
+        }
+
+        return response()->json(['error' => 'You\'re not a tutor'],401);
+    }
+}
