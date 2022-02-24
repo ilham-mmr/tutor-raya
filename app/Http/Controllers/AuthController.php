8d1df4dccfa472dd4fc49f4987e64ec779f6ffc7
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2022-02-24 14:16:42 +0700

    update AuthController for web

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,10 @@ use Laravel\Socialite\Facades\Socialite;
 use GuzzleHttp\Exception\ClientException;
 
 class AuthController extends Controller {
+    public function index() {
+        return view('auth.sign-in');
+    }
+
     /**
      * Redirect the user to the Provider authentication page.
      *
@@ -16,54 +20,14 @@ class AuthController extends Controller {
      * @return JsonResponse
      */
     public function redirectToProvider($provider) {
-        $validated = $this->validateProvider($provider);
-        if (!is_null($validated)) {
-            return $validated;
-        }
-
-        return Socialite::driver($provider)->stateless()->redirect();
-    }
-
-    /**
-     * Obtain the user information from Provider.
-     *
-     * @param $provider
-     * @return JsonResponse
-     */
-    public function handleProviderCallback(Request $request, $provider) {
+        // dd($provider);
 
         $validated = $this->validateProvider($provider);
         if (!is_null($validated)) {
             return $validated;
         }
-        try {
-            $user = Socialite::driver($provider)->stateless()->user();
-        } catch (ClientException $exception) {
-            return response()->json(['error' => 'Invalid credentials provided.'], 422);
-        }
 
-        $userCreated = User::firstOrCreate(
-            [
-                'email' => $user->getEmail()
-            ],
-            [
-                'email_verified_at' => now(),
-                'name' => $user->getName(),
-
-            ]
-        );
-        $userCreated->providers()->updateOrCreate(
-            [
-                'provider' => $provider,
-                'provider_id' => $user->getId(),
-            ],
-            [
-                'avatar' => $user->getAvatar()
-            ]
-        );
-        $token = $userCreated->createToken('tutor-raya')->plainTextToken;
-
-        return response()->json($userCreated, 200, ['Access-Token' => $token]);
+        return Socialite::driver($provider)->with(['state' => 'is_web=true'])->redirect();
     }
 
     /**
@@ -72,19 +36,24 @@ class AuthController extends Controller {
      */
     protected function validateProvider($provider) {
         if (!in_array($provider, ['facebook', 'google'])) {
-            return response()->json(['error' => 'Please login using facebook, or google'], 422);
+            return redirect()->route('sign-in')->with('message','Ooops, There is an error');
         }
     }
 
 
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function logout(Request $request) {
-        // Auth::user()->tokens()->delete();
-        // Revoke the token that was used to authenticate the current request...
-        $request->user()->currentAccessToken()->delete();
+        Auth::logout();
 
+        $request->session()->invalidate();
 
-        return [
-            'message' => 'Logged out'
-        ];
+        $request->session()->regenerateToken();
+
+        return redirect()->route('sign-in')->with('message','You are logged out!');
     }
 }
