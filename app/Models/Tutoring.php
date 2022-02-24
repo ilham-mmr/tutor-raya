95a1b3ba6516a61c71284b2b19b7c8e644fd49df
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2022-02-24 14:18:02 +0700

    add $dates attr and queryScope between - to check the datetime not overlappd

--- app/Models/Tutoring.php
+++ app/Models/Tutoring.php
@@ -5,12 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tutoring extends Model
-{
+class Tutoring extends Model {
     use HasFactory;
 
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'start_time',
+        'end_time'
+    ];
+
+    public function scopeOfBetween($query, $startTime) {
+        return $query->whereRaw("? BETWEEN start_time and end_time", $startTime);
+    }
+
     public function tutor() {
-        return $this->belongsTo(User::class,'tutor_id','id');
+        return $this->belongsTo(User::class, 'tutor_id', 'id');
     }
     public function subject() {
         return $this->belongsTo(Subject::class, 'subject_id', 'id');
